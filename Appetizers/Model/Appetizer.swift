3581cb77d463e3a2f4d531e9756569604cb69e05
saminshakya <saminshakya27@gmail.com>
2025-07-02 11:36:44 +0545

    create order view and add fnction to delete by swiping

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -35,4 +35,34 @@ struct MockData {
                                            carbs: 99)
     
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+    static let orderItemOne = Appetizer(id: 0001,
+                                        name: "Test Appetizer one",
+                                        description: "This is description for appetizer.Its yummy",
+                                        price: 9.99,
+                                        imageURL: "",
+                                        calories: 99,
+                                        protein: 99,
+                                        carbs: 99)
+    static let orderItemTwo = Appetizer(id: 0002,
+                                        name: "Test Appetizer two",
+                                        description: "This is description for appetizer.Its yummy",
+                                        price: 9.99,
+                                        imageURL: "",
+                                        calories: 99,
+                                        protein: 99,
+                                        carbs: 99)
+    
+    static let orderItemThree = Appetizer(id: 0003,
+                                          name: "Test Appetizer three",
+                                          description: "This is description for appetizer.Its yummy",
+                                          price: 9.99,
+                                          imageURL: "",
+                                          calories: 99,
+                                          protein: 99,
+                                          carbs: 99)
+    
+    static let orderItems = [orderItemOne, orderItemTwo, orderItemThree]
+    
+    
 }
--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+
     var body: some View {
         NavigationView {
-            Text("Orders List View")
-                .navigationTitle("🧾 Orders")
-        }    }
+            VStack{
+                List{
+                    ForEach(MockData.orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+                Button {
+                    print("OrderPlaced")
+                } label: {
+                     APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+
+            }
+            .navigationTitle("🧾 Orders")
+        }
+    }
+    func deleteItems(at offsets : IndexSet) {
+        orderItems.remove(atOffsets: offsets)
+    }
 }
 
 #Preview {
