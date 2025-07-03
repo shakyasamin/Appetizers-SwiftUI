9b9a798d0850f73c9d599b60688c83c22f81e191
saminshakya <saminshakya27@gmail.com>
2025-07-03 11:43:47 +0545

    added focus state

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -9,14 +9,37 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedtextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
             Form{
                 Section(header: Text("Personal Information")) {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedtextField, equals: .firstName)
+                        .onSubmit {
+                            focusedtextField = .lastName
+                        }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedtextField, equals: .lastName)
+                        .onSubmit {
+                            focusedtextField = .email
+                        }
+                        .submitLabel(.next)
+
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusedtextField, equals: .email)
+                        .onSubmit {
+                            focusedtextField = nil
+                        }
+                        .submitLabel(.continue)
+
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
@@ -37,6 +60,13 @@ struct AccountView: View {
                 
             }
             .navigationTitle("🤣 Accounts")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Dismiss") {
+                        focusedtextField = nil
+                    }
+                }
+            }
         }
         .onAppear{
             viewModel.retriveUser()
